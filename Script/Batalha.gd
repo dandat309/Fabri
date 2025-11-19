b7ed1e83d6eaa1ce7b043f75a20524803b2f2864
David Belfort <149313325+dandat309@users.noreply.github.com>
2025-11-19 08:16:41 -0300

    Add files via upload

--- Script/Batalha.gd
+++ Script/Batalha.gd
@@ -7,6 +7,8 @@ var StAckcon = 0
 func _ready():
 	$steven2.value = steven.vida
 	$inimigo2.value = enemy.vida
+	await get_tree().create_timer(2.5).timeout
+	$text.text = "" 
 	
 func _on_attack_pressed() -> void:
 	$HBoxContainer/attack.disabled = true
@@ -17,24 +19,40 @@ func _on_attack_pressed() -> void:
 	if enemy.vida <= 0:
 		get_tree().change_scene_to_file("res://Cena/vitoria.tscn")
 		return
-	enemy_turn()
+	await get_tree().create_timer(1.0).timeout
 	$HBoxContainer/attack.disabled = false
-
+	await get_tree().create_timer(1.3).timeout
+	$text.text = "" 
+	enemy_turn()
 func _on_defend_pressed() -> void:
+	$HBoxContainer/attack.disabled = true
 	steven.defender()
 	$text.text = steven.nome +" está defendendo"
+	await get_tree().create_timer(2.0).timeout
+	$HBoxContainer/attack.disabled = false
+	await get_tree().create_timer(1.3).timeout
+	$text.text = "" 
 	enemy_turn()
 
 func _on_heal_pressed() -> void:
 	steven.curar()
 	$text.text = steven.nome +" se curou Vida atual: " + str(steven.vida)
 	$steven2.value = steven.vida
+	await get_tree().create_timer(2.0).timeout
+	$HBoxContainer/attack.disabled = false
+	await get_tree().create_timer(1.3).timeout
+	$text.text = "" 
 	enemy_turn()
 
 func _on_ultimate_pressed() -> void:
+	$HBoxContainer/attack.disabled = false
 	steven.ultimate(enemy)
 	$inimigo2.value = enemy.vida
 	$text.text = steven.nome + " usou seu ultimate \nVida do"  + enemy.nome +  " : " + str(enemy.vida)
+	await get_tree().create_timer(3.0).timeout
+	$HBoxContainer/attack.disabled = true 
+	await get_tree().create_timer(1.3).timeout
+	$text.text = "" 
 	if enemy.vida <= 0:
 		get_tree().change_scene_to_file("res://Cena/vitoria.tscn")
 		return
@@ -43,12 +61,16 @@ func enemy_turn():
 	if StAckcon >= 3:
 		enemy.ultimate(steven)
 		StAckcon = 0
-		$text.text += "\n O " + enemy.nome + " usou a Ult"
+		$text.text += "\n O " + enemy.nome + " usou a Ult" + str(steven.vida)
 		$steven2.value = steven.vida
+		await get_tree().create_timer(1.3).timeout
+		$text.text = "" 
 	else:
 		enemy.atacar(steven)
-		$text.text += "\nO " + enemy.nome + " atacou Steven"
+		$text.text += "\nO " + enemy.nome + " atacou Steven \n Steven ficou com " + str(steven.vida)
 		$steven2.value = steven.vida
+		await get_tree().create_timer(1.3).timeout
+		$text.text = ""
 	if steven.vida <= 0:
 		get_tree().change_scene_to_file("res://Cena/derrota.tscn")
 		
@@ -60,3 +82,7 @@ func enemy_ult():
 	enemy.ultimate(steven)
 	$steven2.value = steven.vida
 	$text.text = "\nO " +enemy.nome + " inimigo usou seu ultimate Vida do Steven: " + str(steven.vida)
+	await get_tree().create_timer(1.5).timeout
+	$text.text = "" 
+	
+	
