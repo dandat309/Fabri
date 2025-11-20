6c9a10abefc6128ea1ced5ec856f27d38a803ac9
David Belfort <149313325+dandat309@users.noreply.github.com>
2025-11-20 18:46:56 -0300

    Add files via upload

--- Script/Batalha.gd
+++ Script/Batalha.gd
@@ -3,86 +3,69 @@ extends Node2D
 var steven = Steven.new()
 var enemy = Enemy.new()
 var StAckcon = 0
-
+var cura = false
+var ult = false
 func _ready():
-	$steven2.value = steven.vida
-	$inimigo2.value = enemy.vida
-	await get_tree().create_timer(2.5).timeout
-	$text.text = "" 
-	
+	$text.text = ""
+
+func trocaTXT():
+	$text.text = ""
+
 func _on_attack_pressed() -> void:
+	trocaTXT()
 	$HBoxContainer/attack.disabled = true
 	steven.atacar(enemy)
 	StAckcon += 1
-	$inimigo2.value = enemy.vida
-	$text.text = steven.nome +" atacou e deixou o " + enemy.nome+ " com " + str(enemy.vida) + " de vida"
+	$text.text = steven.nome + " atacou!\n" + enemy.nome + " agora está com " + str(enemy.vida) + " de vida."
 	if enemy.vida <= 0:
 		get_tree().change_scene_to_file("res://Cena/vitoria.tscn")
 		return
-	await get_tree().create_timer(1.0).timeout
 	$HBoxContainer/attack.disabled = false
-	await get_tree().create_timer(1.3).timeout
-	$text.text = "" 
 	enemy_turn()
+
 func _on_defend_pressed() -> void:
+	trocaTXT()
 	$HBoxContainer/attack.disabled = true
 	steven.defender()
-	$text.text = steven.nome +" está defendendo"
-	await get_tree().create_timer(2.0).timeout
+	$text.text = steven.nome + " está defendendo."
 	$HBoxContainer/attack.disabled = false
-	await get_tree().create_timer(1.3).timeout
-	$text.text = "" 
-	enemy_turn()
 
 func _on_heal_pressed() -> void:
+	trocaTXT()
+	if cura:
+		return
+	cura = true
 	steven.curar()
-	$text.text = steven.nome +" se curou Vida atual: " + str(steven.vida)
-	$steven2.value = steven.vida
-	await get_tree().create_timer(2.0).timeout
-	$HBoxContainer/attack.disabled = false
-	await get_tree().create_timer(1.3).timeout
-	$text.text = "" 
-	enemy_turn()
+	$text.text = steven.nome + " se curou\nVida atual: " + str(steven.vida)
+	await get_tree().create_timer(3).timeout
+	cura = false
 
 func _on_ultimate_pressed() -> void:
-	$HBoxContainer/attack.disabled = false
+	trocaTXT()
+	if ult:
+		return
+	ult = true
 	steven.ultimate(enemy)
-	$inimigo2.value = enemy.vida
-	$text.text = steven.nome + " usou seu ultimate \nVida do"  + enemy.nome +  " : " + str(enemy.vida)
-	await get_tree().create_timer(3.0).timeout
-	$HBoxContainer/attack.disabled = true 
-	await get_tree().create_timer(1.3).timeout
-	$text.text = "" 
+	$text.text = steven.nome + " usou a ULT\n" + enemy.nome + " ficou com " + str(enemy.vida) + " de vida"
 	if enemy.vida <= 0:
 		get_tree().change_scene_to_file("res://Cena/vitoria.tscn")
 		return
+	await get_tree().create_timer(7).timeout
+	ult = false
+	enemy_turn()
 
 func enemy_turn():
+	trocaTXT()
 	if StAckcon >= 3:
 		enemy.ultimate(steven)
 		StAckcon = 0
-		$text.text += "\n O " + enemy.nome + " usou a Ult" + str(steven.vida)
-		$steven2.value = steven.vida
-		await get_tree().create_timer(1.3).timeout
-		$text.text = "" 
+		$text.text = enemy.nome + " usou a ULT\nSteven agora está com " + str(steven.vida) + " de vida."
 	else:
 		enemy.atacar(steven)
-		$text.text += "\nO " + enemy.nome + " atacou Steven \n Steven ficou com " + str(steven.vida)
-		$steven2.value = steven.vida
-		await get_tree().create_timer(1.3).timeout
-		$text.text = ""
+		$text.text = enemy.nome + " atacou!\nSteven agora está com " + str(steven.vida) + " de vida."
 	if steven.vida <= 0:
 		get_tree().change_scene_to_file("res://Cena/derrota.tscn")
-		
+		return
 	if enemy.vida <= 0:
 		get_tree().change_scene_to_file("res://Cena/vitoria.tscn")
 		return
-
-func enemy_ult():
-	enemy.ultimate(steven)
-	$steven2.value = steven.vida
-	$text.text = "\nO " +enemy.nome + " inimigo usou seu ultimate Vida do Steven: " + str(steven.vida)
-	await get_tree().create_timer(1.5).timeout
-	$text.text = "" 
-	
-	
